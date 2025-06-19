526332977a5e37e2e71b03068da368900b21d5f4
Cristian Paul Castañeda <cristianpaul_c@yahoo.com>
2025-06-19 23:17:24 +0800

    V2.3 | Add new page - extensions

---
+++ post/extensions.php
@@ -0,0 +1,64 @@
+<?php include __DIR__ . "/../include/head.php" ?>
+<?php include __DIR__ . "/../include/header.php" ?>
+
+<article class="tdc--writing__article">
+    <div class="inner">
+
+        <div class="tdc--toggle__container">
+        <p><a href="../index.php" class="tdc--transition__link"><i class="fa-solid fa-arrow-left"></i> back to
+            homepage</a>
+        </p>
+        </div>
+
+        <div class="tdc--about__box">
+
+            <div class="tdc--extensions__box extension--description">
+           
+                <h3>VS Code Extensions</h3>
+                
+                <div class="extension--box">
+                    <h4>Environment</h4>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer">Live Server</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=GitHub.copilot">GitHub CoPilot</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=eamodio.gitlens">GitLens</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode">Prettier</a></p>
+
+                </div>
+                <div class="extension--box">
+                    <h4>Development</h4>
+
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=abusaidm.html-snippets">HTML5 Snippets</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=pranaygp.vscode-css-peek">CSS Peek</a></p>
+    
+                </div>
+                <div class="extension--box">
+                    <h4>Scripting</h4>
+
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint">ESLint</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets">ES7+ React/Redux/React-Native snippets</a></p>
+                </div>
+
+                <div class="extension--box">
+                    <h4>Objects</h4>
+
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=Meezilla.json">Beautify JSON</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=ClemensPeters.format-json">JSON Formatter</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=haknkayaa.json-visualizer">JSON Visualizer</a></p>
+                </div>
+
+                <div class="extension--box">
+                    <h4>Files</h4>
+
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=mechatroner.rainbow-csv">Rainbow CSV</a></p>
+                    <p>- <a href="https://marketplace.visualstudio.com/items?itemName=christian-kohler.path-intellisense">Path Intellisense</a></p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</article>
+
+<?php include __DIR__ . "/../include/footer.php" ?>
--- temp/about.php
+++ temp/about.php
@@ -3,11 +3,14 @@
     <article class="tdc--about__description">
       <p>I'm Cristian Paul, I continuously explore modern frontend and backend tools to streamline development workflows.</p>
 
-        <p>
-          <a class="tdc--transition__link" href="<?=$base_url ?>post/about-thedevcristian.php">About <i class="fa-regular fa-circle-user"></i></a>
+        <p>>
+          <a class="tdc--transition__link" href="<?=$base_url ?>post/about-thedevcristian">About </a>
         </p>
-        <p>
-          <a class="tdc--transition__link" href="<?=$base_url ?>post/tools-bookmark">Tools <i class="fa-regular fa-keyboard"></i></a>
+        <p>>
+          <a class="tdc--transition__link" href="<?=$base_url ?>post/tools-bookmark">Tools </a>
+        </p>
+        <p>>
+          <a class="tdc--transition__link" href="<?=$base_url ?>post/extensions">Extensions & Others </a>
         </p>
     </article>
   </div>
